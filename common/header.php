18e42b15c98742172058c095b19abd785615c7e3
Createrix <omalka@createrixlabs.com>
2019-11-25 19:34:51 +0530

    layout edit 1

---
+++ common/header.php
@@ -0,0 +1,32 @@
+<meta charset="UTF-8">
+<!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/><![endif]-->
+
+<title>Ravana Heights | Official Website</title>
+
+<meta name="author" content="Createrix Labs 2019 | Omalka Menuwan"> 
+<meta name="description" content="Ravana Heights is, an elegant Sri Lankan boutique hotel, situated on the top of the Ella-pass, in the picturesque village of Ella, at an elevation of 3300ft, looking at the vast coastal plain below, as far as the eye can reach! We have very comfortable accommodation with a reputation of excellent Sri Lankan- Thai cuisine, personalized service with friendly and helpful hosts.Enjoy the tranquillity set in an artistically landscaped garden, rich with a collection of exotic tropical plants. The variation of magnificent colours reaches its peak at sunset..... In Ella, you will experience the diversity of nature at its best." />
+<meta name="keywords" content="Ella, Ella Sri Lanka, sri lanka, tourism, boutique hotels, hotels in ella, boutique hotels in ella, pradise ella, best place to stay in ella"/>
+<meta name='copyright' content='All Copyrights reserved at Ravana Heights 2019'>
+
+<!-- Facebook Open Graph Meta
+================================================== -->
+<meta property="og:image" content="images/facebook/og-image.jpg">
+<meta property="og:image:width" content="279">
+<meta property="og:image:height" content="279">
+<meta property="og:title" content="Ravana Heights | Ella">
+<meta property="og:description" content="Ravana Heights is, an elegant Sri Lankan boutique hotel, situated on the top of the Ella-pass, in the picturesque village of Ella, at an elevation of 3300ft, looking at the vast coastal plain below, as far as the eye can reach! We have very comfortable accommodation with a reputation of excellent Sri Lankan- Thai cuisine, personalized service with friendly and helpful hosts.Enjoy the tranquillity set in an artistically landscaped garden, rich with a collection of exotic tropical plants. The variation of magnificent colours reaches its peak at sunset..... In Ella, you will experience the diversity of nature at its best.">
+<meta property="og:url" content="http://ravanaheights.com">
+
+<!-- Mobile
+================================================== -->
+<meta name="viewport" content="width=device-width, initial-scale = 1, maximum-scale=1, user-scalable=no" />				
+
+<!-- Fav and iconsimages/favicon
+================================================== -->	
+
+
+
+
+<!--CSS-->
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
-<?php
-include 'content/home.php';
-include 'content/rooms.php';
-include 'content/gallery.php';
-include 'content/reviews.php';
-include 'content/booknow.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!--Include Header File-->
+		<?php include 'common/header.php'; ?>
+
+	</head>
+	<body>
+		<div id="wrapper">
+			<?php
+			include 'content/home.php';
+			include 'content/rooms.php';
+			include 'content/gallery.php';
+			include 'content/reviews.php';
+			include 'content/booknow.php';
+			?>
+		</div>
+	</body>
+</html>	

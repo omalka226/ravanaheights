04b9414dfc29893b98a262b10ca8e170042d0a70
Createrix <omalka@createrixlabs.com>
2019-11-26 11:28:11 +0530

    home section chnages

--- content/home.php
+++ content/home.php
@@ -1,1 +1,11 @@
-<section id="home" class="frame-container"></section>
+<section id="home" class="frame-container">
+	<div class="inner-container">
+		<div id="home-description-container">
+			<img src="images/logo.png" alt="ravanaheights logo"/>
+			<p>Ravana Heights is, an elegant Sri Lankan boutique hotel, situated on the top of the Ella-pass, in the picturesque village of Ella, at an elevation of 3300ft, looking at the vast coastal plain below, as far as the eye can reach! We have very comfortable accommodation with a reputation of excellent Sri Lankan- Thai cuisine, personalized service with friendly and helpful hosts.Enjoy the tranquillity set in an artistically landscaped garden, rich with a collection of exotic tropical plants. The variation of magnificent colours reaches its peak at sunset..... In Ella, you will experience the diversity of nature at its best.</p>
+			<p>Our hotel is a combination of luxurious indoor living, with tranquil or adventurous outdoor life.</p>
+		</div>
+		<h3>Luxurious living with warm hospitality</h3>
+	</div>
+	<img src="images/mouse.png" class="mouse"/>
+</section>

464e5e52484f27bed1f4bf8c02e192a6241cbc55
Createrix <omalka@createrixlabs.com>
2019-11-26 20:31:38 +0530

    Footer styling change

--- common/footer.php
+++ common/footer.php
@@ -1,1 +1,22 @@
-<footer></footer>
+<footer>
+	<div class="footer-inner">
+		<div class="footer-column">
+			<h2 class="footer-title">Address</h2>
+			<p>Opposite 27/6 kilometer Marker,<br>Wellawaya Road,<br>Ella,<br>Sri Lanka</p>
+		</div>
+		<div class="footer-column">
+			<h2 class="footer-title">Telephone</h2>
+			<p>+94(0) 57 222 8888<br>+94(0) 77 730 9542</p>
+			<h2 class="footer-title">Email</h2>
+			<p><a href="mailto:info@ravanahaheights.com">info@ravanaheights.com</a></p>
+		</div>
+		<div class="footer-column">
+			<h2 class="footer-title">Location</h2>
+			<div id="map"></div>
+		</div>
+	</div>
+	<div class="footer-bottom">
+		<span>Copyright 2019, Ravana Heights, All Rights Reserved</span>
+		<span>Designed by</span>
+	</div>
+</footer>

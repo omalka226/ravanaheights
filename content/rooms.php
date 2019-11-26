9dc1e9bdddaf6c1788b10364eb7d4fcfa5d191e3
Createrix <omalka@createrixlabs.com>
2019-11-26 13:22:29 +0530

    room section edit 01

--- content/rooms.php
+++ content/rooms.php
@@ -1,16 +1,24 @@
 <section id="rooms" class="frame-container">
-	<h2 class="title">Rooms</h2>
-	<div class="room-container">
-		<div class="room-image-container"></div>
-		<div class="room-detail-container">
-			<div class="room-detail-upper">
-				<div class="room-detail-titles">
-					<h2 class="room-title">Deluxe Double</h2>
-					<span class="max-occupancy"></span>
-				</div>
-				<span class="room-price">$ 85</span>
+	<div class="inner-container">
+		<div class="main-info-container">
+			<h2 class="title">Rooms</h2>
+			<div class="controllers-rooms">
+				<img src="images/left.png" class="controller"/>
+				<img src="images/right.png" class="controller"/>
 			</div>
-			<p>Ground floor 25 Sq Meter room with en suite bathroom with Grohe pressure showers, and private sit-out.. The rooms have floor to ceiling sliding doors on the ground flooor, with great views of Ella rock; and the Ella gap from the private sit-out, which is in the flower garden. Two of the three standard rooms have king size beds, with very luxurious...</p>
+		</div>
+		<div class="room-container">
+			<div class="room-image-container"></div>
+			<div class="room-detail-container">
+				<div class="room-detail-upper">
+					<div class="room-detail-titles">
+						<h2 class="room-title">Deluxe Double</h2>
+						<span class="max-occupancy"></span>
+					</div>
+					<span class="room-price">$ 85</span>
+				</div>
+				<p>Ground floor 25 Sq Meter room with en suite bathroom with Grohe pressure showers, and private sit-out.. The rooms have floor to ceiling sliding doors on the ground flooor, with great views of Ella rock; and the Ella gap from the private sit-out, which is in the flower garden. Two of the three standard rooms have king size beds, with very luxurious...</p>
+			</div>
 		</div>
 	</div>
 </section>

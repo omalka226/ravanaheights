6fffd03377060ada53bbc6c5429670ca3e1b0fc9
Createrix <omalka@createrixlabs.com>
2019-11-27 13:54:41 +0530

    edit review section

--- content/reviews.php
+++ content/reviews.php
@@ -1,5 +1,20 @@
 <section id="reviews" class="frame-container">
 	<div class="inner-container">
-		<h2 class="title">Happy Guests</h2>
+		<div class="review-container">
+			<div class="review-wrapper">
+				<div class="review-image">
+					<h3 class="reviewer-name"></h3>
+					<h3 class="reviewer-country"></h3>
+				</div>
+				<div class="review-body">
+					<h2 class="review-title"></h2>
+					<p></p>
+					<span class="hashtags"></span>
+				</div>
+			</div>
+		</div>
+		<div class="review-header">
+			<h2 class="title">Happy Guests</h2>
+		</div>
 	</div>
 </section>
